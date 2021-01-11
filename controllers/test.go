89efe0e7f38c575b50c46f609f99b58c2df7b95d
LAPTOP-2RJED8F0\acmtt <xiyue9311@gmail.com>
2021-01-11 16:28:05 +0800

    新增測試 recover()

--- controllers/test.go
+++ controllers/test.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"errors"
 	"fmt"
 
 	"github.com/gin-gonic/gin"
@@ -10,4 +11,22 @@ type TestController struct{}
 
 func (tc *TestController) Test(c *gin.Context) {
 	fmt.Println("test router.")
+	var err error
+	defer func() {
+		if r := recover(); r != nil {
+			switch x := r.(type) {
+			case string:
+				err = errors.New(x)
+			case error:
+				err = x
+			default:
+				err = errors.New("Unknown panic!")
+			}
+		} else {
+			fmt.Println("commit.")
+		}
+	}()
+
+	// panic("a")
+	return
 }
