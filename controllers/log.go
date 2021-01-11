11b3833bd98f7d4c001be1ac6c2669265568165c
LAPTOP-2RJED8F0\acmtt <xiyue9311@gmail.com>
2021-01-11 16:28:55 +0800

    新增data logger controller

--- controllers/log.go
+++ controllers/log.go
@@ -7,10 +7,15 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
-	"strconv"
+
+	"logOperation/setting"
 
 	"github.com/gin-gonic/gin"
 )
+
+var dcTr *http.Transport
+
+var ip string
 
 type LogController struct{}
 
@@ -26,35 +31,25 @@ type logOutput struct {
 	TFst  int `json:"TFst"`
 }
 
+func init() {
+	ip = setting.IP()
+}
+
+func setReqHeader(req *http.Request) {
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Authorization", "Basic cm9vdDptaXRyb290")
+	req.Header.Set("Cache-Control", "no-store")
+}
+
 func (lc *LogController) UpdateOutput(c *gin.Context) {
 
-	UID, _ := strconv.Atoi(c.Query("UID"))
-	MAC, _ := strconv.Atoi(c.Query("MAC"))
-	Fltr, _ := strconv.Atoi(c.Query("Fltr"))
-	TSt, _ := strconv.Atoi(c.Query("TSt"))
-	TEnd, _ := strconv.Atoi(c.Query("TEnd"))
-	Amt, _ := strconv.Atoi(c.Query("Amt"))
-	Total, _ := strconv.Atoi(c.Query("Total"))
-	TLst, _ := strconv.Atoi(c.Query("TLst"))
-	TFst, _ := strconv.Atoi(c.Query("TFst"))
+	data, _ := ioutil.ReadAll(c.Request.Body)
 
-	input := logOutput{
-		UID:   UID,
-		MAC:   MAC,
-		Fltr:  Fltr,
-		TSt:   TSt,
-		TEnd:  TEnd,
-		Amt:   Amt,
-		Total: Total,
-		TLst:  TLst,
-		TFst:  TFst,
-	}
-	fmt.Printf("input: %#v\n", input)
-
-	jsonStrBytes, err := json.Marshal(input)
+	fmt.Printf("data: %s\n", data)
 
 	client := &http.Client{}
-	req, err := http.NewRequest(http.MethodPut, "http://192.168.10.145:80/log_output", bytes.NewBuffer(jsonStrBytes))
+	req, err := http.NewRequest(http.MethodPut, ip+"/log_output", bytes.NewBuffer(data))
+	setReqHeader(req)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -66,14 +61,35 @@ func (lc *LogController) UpdateOutput(c *gin.Context) {
 
 func (lc *LogController) GetOutput(c *gin.Context) {
 
-	response, err := http.Get("http://192.168.10.145:80/log_output")
+	req, err := http.NewRequest(http.MethodGet, ip+"/log_output", nil)
+	setReqHeader(req)
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Println("field to connnect to get log output.")
+	}
 
-	// defer response.Body.Close()
-	bodyBytes, err := ioutil.ReadAll(response.Body)
+	bodyBytes, err := ioutil.ReadAll(resp.Body)
 
 	result := logOutput{}
 	if err = json.Unmarshal(bodyBytes, &result); err != nil {
 		return
 	}
 	fmt.Printf("log_output: %#v\n", result)
+}
+
+func (lc *LogController) LogMessage(c *gin.Context) {
+
+	req, err := http.NewRequest(http.MethodGet, ip+"/log_message", nil)
+	setReqHeader(req)
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Println("field to connnect to get log message.")
+	}
+
+	// defer response.Body.Close()
+	bodyBytes, err := ioutil.ReadAll(resp.Body)
+
+	fmt.Printf("log_message: %s\n", bodyBytes)
 }
