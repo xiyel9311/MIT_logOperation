88c88094fb09405d65f71b958682f944945c8ff5
LAPTOP-2RJED8F0\acmtt <xiyue9311@gmail.com>
2021-01-13 08:54:15 +0800

    sysparam 新增 ThisPort, MainIP, SensorComNum, TempratureChNum, HumidityChNum

--- models/sysparam/sysparam.go
+++ models/sysparam/sysparam.go
@@ -1,5 +1,11 @@
 package sysparam
 
 const (
-	IP string = "http://192.168.10.188"
+	ThisPort string = "8885"
+
+	MainIP string = "http://192.168.10.188"
+
+	SensorComNum    int = 1
+	TempratureChNum int = 0
+	HumidityChNum   int = 1
 )
